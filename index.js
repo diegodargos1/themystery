3eaa8535f175b9c54499cc5e3a1893149672e9f8
DDargos <diegodargos@gmail.com>
2020-05-13 15:22:43 -0700

    second

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const routes = require("./src/routes");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+mongoose.connect(
+  //"mongodb+srv://dargos:mat333@cluster0-v9sov.mongodb.net/initial?retryWrites=true&w=majority",
+  "mongodb+srv://dargos:mat333@cluster0-uc4kp.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+app.use(cors());
+app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(routes);
+
+app.listen(PORT);
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(
   //"mongodb+srv://dargos:mat333@cluster0-v9sov.mongodb.net/initial?retryWrites=true&w=majority",
@@ -15,4 +16,4 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(8080);
+app.listen(PORT);
